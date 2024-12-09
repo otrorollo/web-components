f048112e353ebbb2ebc64e9c3c7627306fba448e
RdRubén <rubenponcebusiness@gmail.com>
2024-12-09 21:58:38 +0100

    clase 1 - ejercicio 3 completo

--- 1.3_ES-Modules/solution3/digital-clock.js
+++ 1.3_ES-Modules/solution3/digital-clock.js
@@ -2,3 +2,25 @@
   1- Create a digital-clock component.
   2- Each second, we must calculate the time and update the component HTML
 */
+
+class DigitalClock extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    setInterval(() => {
+      this.innerHTML = this.getTime();
+    }, 1000);
+  }
+  getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `<h2>${hours}:${minutes}:${seconds}</h2>`;
+    }
+  }
+
+window.customElements.define("digital-clock", DigitalClock);
